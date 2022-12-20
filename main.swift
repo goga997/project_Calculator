711da7e51e370ba7cd6e820ad805a2a96ce5d84e
goga997 <119938172+goga997@users.noreply.github.com>
2022-12-20 18:06:00 +0200

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,43 +1,76 @@
-print("Welcome to the Calculator")
-let operation = getInfo(description: "Introduceti semnul operatiunei: +, -, *, /")
-let number1 = getInfo(description: "Alegeti primul numar")
-let number2 = getInfo(description: "Alegeti al doilea numar")
+import Darwin
 
-print("Calculation... ->  " + number1 + " " + operation + " " + number2)
+print("WELCOME to the G_Calculator")
+var history:[String] = []
 
-if let number1 = Int(number1) {
-    if let number2 = Int(number2) {
-         calculate(operation: operation, number1: number1, number2: number2)
-    } else {
-        print("Ati introdus gresit al doiela numar")
+
+while true {
+    
+    let action = getInfo(info: "Choose action:\nCalculate -> C\nExit -> Q\nHistory -> H")
+    switch action {
+    case "c": calculate()
+    case "q": exit (0)
+    case "h": History()
+    default: print("This type of action is unavailable")
     }
     
-} else {
-    print("Ati introdus gresit primul numar")
+    print("-----------------------------")
 }
+    
 
-
-func getInfo(description: String) -> String {
-    print(description)
+func getInfo(info: String) -> String {
+    print(info)
     return readLine() ?? ""
 }
 
-func showResult(_ result: Int) {
-    let result = String(result)
-    print("Rezultatul este " + result)
+func calculate() {
+    let operation = getInfo(info: "Chose operation signe: +, -, /, *")
+    guard operation == "+" || operation == "-" || operation == "*" || operation == "/" else {
+        return
+    }
+    
+    let number1 = getInfo(info: "Enter first number")
+    guard let number1 = Int(number1) else {
+        print("Failed (first number)")
+        return
+    }
+    
+    let number2 = getInfo(info: "Enter second number")
+    guard let number2 = Int(number2) else {
+        print("Failed (second number")
+        return
+    }
+    
+    let half = String(number1) + " " + operation + " " + String(number2)
+    print("Calculation:  " + half)
+    
+    let result = calculate(number1: number1, number2: number2, operation: operation)
+    guard let result = result else { return }
+    showRes(result)
+    history.append(half + " = " + String(result))
 }
 
-func calculate(operation: String, number1: Int, number2: Int) {
+
+func calculate(number1 first: Int, number2 second: Int, operation: String) -> Int? {
     switch operation {
-    case "+": showResult(number1 + number2)
-    case "-": showResult(number1 - number2)
-    case "*": showResult(number1 * number2)
-    case "/":  if number2 != 0 {
-        showResult(number1 / number2)
-    } else {
-        print("Impartirea la 0 nu poate avea loc")
-    }
-    default: print("Ati introdus gresit semnul operatiunii")
+    case "+":  return first + second
+    case "-": return first - second
+    case "*": return first * second
+    case "/" where second == 0:
+        print("Impossible with 0")
+        return nil
+    case "/": return first / second
+    default: print("You choose wrong signe of operation")
+        return nil
     }
 }
 
+func showRes(_ result: Int) {
+    print("Result is  " + String(result))
+}
+
+func History() {
+    for half in history {
+        print(half)
+    }
+}
